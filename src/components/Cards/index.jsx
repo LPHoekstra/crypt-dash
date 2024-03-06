406992fb209110a5d00f53cf95ed2d016f1deaee
MalBourbie <MalBourbie@gmail.com>
2024-03-06 20:18:40 +0100

    Slide et backgrounds des Cards

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom"
 import styled from "styled-components"
 import colors from "../../styles/colors"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { FreeMode } from "swiper/modules"
+import "swiper/css/free-mode"
+import "swiper/css"
 
 const HeadingContenair = styled.div`
   display: flex;
@@ -12,6 +16,23 @@ const SeeAll = styled(Link)`
   text-decoration: underline;
   color: ${colors.primary2};
 `
+const PremiereCards = styled.div`
+  background-color: ${colors.primary3};
+  height: 100%;
+  width: 100%;
+  border-radius: 15px;
+`
+
+const DeuxiemeCards = styled.div`
+  border: solid 1px #dfeaf2;
+  border-radius: 15px;
+  height: 100%;
+  width: 100%;
+`
+
+const Slide = styled(SwiperSlide)`
+  height: 170px;
+`
 
 function Cards() {
   return (
@@ -20,6 +41,19 @@ function Cards() {
         <h2>My Cards</h2>
         <SeeAll to="">See All</SeeAll>
       </HeadingContenair>
+      <Swiper
+        spaceBetween={20}
+        width={265}
+        freeMode={true}
+        modules={[FreeMode]}
+      >
+        <Slide>
+          <PremiereCards>1</PremiereCards>
+        </Slide>
+        <Slide>
+          <DeuxiemeCards>2</DeuxiemeCards>
+        </Slide>
+      </Swiper>
     </div>
   )
 }
