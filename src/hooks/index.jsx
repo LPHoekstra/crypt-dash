38e4fa481caa6add4b528298f76b515959c11c27
MalBourbie <MalBourbie@gmail.com>
2024-03-15 14:41:18 +0100

    ajout d'une condition pour savoir si l'utilisateur est connecté. Si il est, le fetch sera lancé

--- src/hooks/index.jsx
+++ src/hooks/index.jsx
@@ -1,35 +1,39 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import Cookies from "js-cookie"
+import { ConnectedContext } from "../context"
 
 export function useFetch(url) {
   const [data, setData] = useState({})
   const [isLoading, setLoading] = useState(true)
   const [error, setError] = useState(false)
   const token = Cookies.get("token")
+  const { isConnected } = useContext(ConnectedContext)
 
   useEffect(() => {
     if (!url) return
     setLoading(true)
     async function fetchData() {
-      try {
-        const response = await fetch(url, {
-          method: "GET",
-          headers: {
-            Authorization: `${token}`,
-          },
-        })
-        const data = await response.json()
-        setData(data)
-      } catch (err) {
-        console.log(err)
-        setError(true)
-      } finally {
-        setLoading(false)
+      if (isConnected === true) {
+        try {
+          const response = await fetch(url, {
+            method: "GET",
+            headers: {
+              Authorization: `${token}`,
+            },
+          })
+          const data = await response.json()
+          setData(data)
+        } catch (err) {
+          console.log(err)
+          setError(true)
+        } finally {
+          setLoading(false)
+        }
       }
     }
 
     fetchData()
-  }, [url, token])
+  }, [url, token, isConnected])
   return { isLoading, data, error }
 }
 
